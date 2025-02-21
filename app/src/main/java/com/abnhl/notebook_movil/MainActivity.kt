c4fae80c2be5c85ea0bacfc60804cdd0eeb7a9f5
AbnHL-24 <abner1269@outlook.com>
2025-02-21 06:19:12 -0600

    Commit de inicio.

---
+++ app/src/main/java/com/abnhl/notebook_movil/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.abnhl.notebook_movil
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.abnhl.notebook_movil.ui.theme._20251SCompi1Practica1NotebookMovilTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _20251SCompi1Practica1NotebookMovilTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _20251SCompi1Practica1NotebookMovilTheme {
+        Greeting("Android")
+    }
+}

d62ee0787e800d93b5959441b6c2121c08b5aab1
AbnHL-24 <abner1269@outlook.com>
2025-02-27 10:19:41 -0600

    Entrada de texto

--- app/src/main/java/com/abnhl/notebook_movil/MainActivity.kt
+++ app/src/main/java/com/abnhl/notebook_movil/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.abnhl.notebook_movil.ui.TipoEntrada
 import com.abnhl.notebook_movil.ui.theme._20251SCompi1Practica1NotebookMovilTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,15 +35,77 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             _20251SCompi1Practica1NotebookMovilTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ViewContainer()
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+//@Preview
+@Composable
+fun ViewContainer(InnerPadding: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Notebook Móvil") },
+
+                )
+        },
+    ) { innerPadding ->
+        GestionDeEntradas(Modifier.padding(innerPadding))
+    }
+}
+
+@Preview
+@Composable
+fun GestionDeEntradas(modifier: Modifier = Modifier) {
+    var entradas = rememberSaveable { mutableStateListOf<@Composable () -> Unit>() }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        LazyRow(horizontalArrangement = Arrangement.SpaceBetween) {
+            item {
+                ElevatedButton(onClick = {
+                    entradas.add {
+                        Entrada(
+                            TipoEntrada.TEXTO,
+                            modifier
+                        )
+                    }
+                }) { Text("Texto") }
+            }
+            item {
+                ElevatedButton(onClick = {
+                    entradas.add {
+                        Entrada(
+                            TipoEntrada.CODIGO,
+                            modifier
+                        )
+                    }
+                }) { Text("Código") }
+            }
+        }
+        LazyColumn {
+            items(entradas) { entrada ->
+                entrada()
+            }
+        }
+    }
+}
+
+@Composable
+fun Entrada(tipoEntrada: TipoEntrada, modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+fun EntradaPreview() {
+    Entrada(TipoEntrada.TEXTO)
 }
 
 @Composable
@@ -38,10 +116,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ViewContainerPreview() {
     _20251SCompi1Practica1NotebookMovilTheme {
-        Greeting("Android")
+        ViewContainer()
     }
 }
---
+++ app/src/main/java/com/abnhl/notebook_movil/ui/TipoEntrada.kt
@@ -0,0 +1,5 @@
+package com.abnhl.notebook_movil.ui
+
+enum class TipoEntrada {
+    TEXTO, CODIGO
+}
